91752f01a9db3d3adb04a6fdbd86191f199519cf
Martin Unterhofer <martin.unter@icloud.com>
2025-03-08 14:35:01 +0100

    2025-03-08 14:03:03

--- Zadaci/Zadatak-01/z1.py
+++ Zadaci/Zadatak-01/z1.py
@@ -3,10 +3,10 @@
 # Loading input files
 poravnanje_file = open("por001.txt", "r")
 matrica_file = open("blosum50.txt", "r")
-kiseilne_file = open("acids.txt", "r")
+kiseline_file = open("acids.txt", "r")
 
 # Interpreting loaded data
-poravnanje = [poravnanje_file.readline(), poravnanje_file.readline()]
+poravnanje = [poravnanje_file.readline().strip('\n'), poravnanje_file.readline().strip('\n')]
 
 matrica = [[0]*20 for i in range (20)]
 
@@ -15,3 +15,14 @@ for i in range (20):
     for j in range(20):
         matrica[i][j] = int(redak[j])
 
+kiseline = kiseline_file.readline()
+
+# Calculating sum
+sum = 0
+for i in range(len(poravnanje[0])):
+    if (poravnanje[0][i] == '_' or poravnanje[1][i] == '_'):
+        sum-=8
+    else:
+        sum += matrica[kiseline.index(poravnanje[0][i])][kiseline.index(poravnanje[1][i])]
+
+print (sum)
