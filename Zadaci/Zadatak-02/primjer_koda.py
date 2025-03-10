6f93ee41d4f37686a656b1ebb1a736ba845be4da
Martin Unterhofer <martin.unter@icloud.com>
2025-03-10 08:48:20 +0100

    2025-03-10 08:40:00

---
+++ Zadaci/Zadatak-02/primjer_koda.py
@@ -0,0 +1,45 @@
+f1=open("acids.txt", "r")
+ak=f1.readline()
+f1.close()
+
+bm=[]
+f1=open("blosum50.txt", "r")
+for i in range(20):
+    line=f1.readline()
+    vc=line.split()
+    bm.append(vc[:])
+for i in range(20):
+    for j in range(20):
+        bm[i][j]=int(bm[i][j])
+f1.close()
+
+x='HPEW'
+y='PW'
+m=len(x)
+n=len(y)
+
+sm=[] 
+tmp=[]
+for i in range(m+1):
+    tmp.append(0)
+for i in range(n+1):
+    sm.append(tmp[:])
+
+
+for i in range(n+1):
+    sm[i][0]=-8*i
+for i in range(m+1):
+    sm[0][i]=-8*i
+
+for i in range(1,n+1):
+    for j in range(1,m+1):
+        tmp=[]
+        tmp.append(sm[i-1][j]-8)
+        tmp.append(sm[i][j-1]-8)
+        bb=bm[ak.index(y[i-1])][ak.index(x[j-1])]
+        tmp.append(sm[i-1][j-1]+bb)
+        tmp.sort()
+        sm[i][j]=tmp[2]
+
+
+
